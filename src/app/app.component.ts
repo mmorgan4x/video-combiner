b4a0301c4a4063e17b2700bbc6813ce0b39e3c2a
Matt Morgan <matt.morgan@cmgfi.com>
2023-11-30 03:23:37 -0500

    added some styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { LogEvent } from '@ffmpeg/ffmpeg/dist/esm/types';
 import { FFmpeg } from 'node_modules/@ffmpeg/ffmpeg';
 import { fetchFile, toBlobURL } from 'node_modules/@ffmpeg/util';
 
@@ -9,30 +10,38 @@ import { fetchFile, toBlobURL } from 'node_modules/@ffmpeg/util';
 })
 export class AppComponent {
 
-  loaded = false;
+  videoUrl = 'https://raw.githubusercontent.com/ffmpegwasm/testdata/master/Big_Buck_Bunny_180_10s.webm'
+  loading = true;
   ffmpeg = new FFmpeg();
   message = '';
-  src = '';
+  src = this.videoUrl;
+  cmd = '-i input.webm -vf hue=s=0 output.mp4';
+  @ViewChild('code') code: ElementRef<HTMLElement>;
 
-  async load() {
-    const baseURL = 'https://unpkg.com/@ffmpeg/core@0.12.4/dist/umd'
-    this.ffmpeg.on('log', ({ message }) => {
-      this.message += (message + '\n');
-      console.log(message);
-    });
-    // toBlobURL is used to bypass CORS issue, urls with the same
-    // domain can be used directly.
+  async ngOnInit() {
+    this.ffmpeg.on('log', t => this.writeLine(t.message));
+    const baseURL = 'https://unpkg.com/@ffmpeg/core@0.12.4/dist/umd';
     await this.ffmpeg.load({
       coreURL: await toBlobURL(`${baseURL}/ffmpeg-core.js`, 'text/javascript'),
       wasmURL: await toBlobURL(`${baseURL}/ffmpeg-core.wasm`, 'application/wasm'),
     });
-    this.loaded = true;
+    this.loading = false;
   }
 
-  async transcode() {
-    await this.ffmpeg.writeFile('input.webm', await fetchFile('https://raw.githubusercontent.com/ffmpegwasm/testdata/master/Big_Buck_Bunny_180_10s.webm'));
-    await this.ffmpeg.exec(['-i', 'input.webm', 'output.mp4']);
+  async execute() {
+    // this.loading = true;
+    this.writeLine('');
+    this.writeLine(this.cmd);
+    await this.ffmpeg.writeFile('input.webm', await fetchFile(this.videoUrl));
+    await this.ffmpeg.exec(this.cmd.split(' '));
     const data = await this.ffmpeg.readFile('output.mp4') as Uint8Array;
     this.src = URL.createObjectURL(new Blob([data.buffer], { type: 'video/mp4' }));
+    this.loading = false;
+  }
+
+  writeLine(msg: string) {
+    this.message += (msg + '\n');
+    console.log(msg);
+    this.code.nativeElement.scrollTop = this.code?.nativeElement.scrollHeight;
   }
 }
